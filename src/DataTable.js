cdc9d9217da5596f9a38434cac383b819a796ac9
Julian Kingman <julian@designman.io>
2017-03-21 19:15:33 -0400

    debugging

--- src/DataTable.js
+++ src/DataTable.js
@@ -22,6 +22,7 @@ export function DataTable(props) {
     refCallback,
     renderRow,
     scrollable,
+    contentContainerStyle,
     ...listViewProps,
   } = props;
   const renderListView = (props) => (
@@ -37,7 +38,7 @@ export function DataTable(props) {
     <View>
       {
         scrollable ?
-          <ScrollView style={[defaultStyles.verticalContainer, style]} horizontal>
+          <ScrollView contentContainerStyle={contentContainerStyle} style={[defaultStyles.verticalContainer, style]} horizontal>
             {typeof renderHeader === 'function' && renderHeader()}
             {renderListView()}
           </ScrollView> :
