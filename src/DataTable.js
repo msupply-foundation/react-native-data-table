0c259a8a364e5839a0234fb0331640a7b8f564b3
andreievg <andrei.evguenov@gmail.com>
2017-09-12 13:54:49 +1200

    Re render only when dataSource changes

--- src/DataTable.js
+++ src/DataTable.js
@@ -15,28 +15,40 @@ import {
 
 import { ListView } from 'realm/react-native';
 
-export function DataTable(props) {
-  const {
-    style,
-    listViewStyle,
-    renderHeader,
-    dataSource,
-    refCallback,
-    renderRow,
-    ...listViewProps,
-  } = props;
-  return (
-    <View style={[defaultStyles.verticalContainer, style]}>
-      {typeof renderHeader === 'function' && renderHeader()}
-      <ListView
-        {...listViewProps}
-        ref={refCallback}
-        style={[defaultStyles.listview, listViewStyle]}
-        dataSource={dataSource}
-        renderRow={renderRow}
-      />
-    </View>
-  );
+export class DataTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
+  }
+
+  shouldComponentUpdate(props) {
+    return this.props.dataSource !== props.dataSource;
+  }
+
+  render() {
+    const {
+      style,
+      listViewStyle,
+      renderHeader,
+      dataSource,
+      refCallback,
+      renderRow,
+      ...listViewProps,
+    } = this.props;
+
+    return (
+      <View style={[defaultStyles.verticalContainer, style]}>
+        {typeof renderHeader === 'function' && renderHeader()}
+        <ListView
+          {...listViewProps}
+          ref={refCallback}
+          style={[defaultStyles.listview, listViewStyle]}
+          dataSource={dataSource}
+          renderRow={renderRow}
+        />
+      </View>
+    );
+  }
 }
 
 DataTable.propTypes = {
