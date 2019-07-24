61245714e09aea043af4b74ef76b788eb6789520
Chris Petty <christopher.m.petty@gmail.com>
2019-07-24 16:37:37 +1200

    Update DataTable.js

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,4 +1,3 @@
-
 /* @flow weak */
 
 /**
@@ -6,70 +5,33 @@
  * Sustainable Solutions (NZ) Ltd. 2016
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import React from 'react'
 import {
   StyleSheet,
-  View,
-  ViewPropTypes,
-} from 'react-native';
+  VirtualizedList,
+  VirtualizedListPropTypes,
+} from 'react-native'
 
-import { ListView } from 'realm/react-native';
-
-export class DataTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
-  }
-
-  shouldComponentUpdate(props) {
-    return this.props.dataSource !== props.dataSource;
-  }
-
-  render() {
-    const {
-      style,
-      listViewStyle,
-      renderHeader,
-      dataSource,
-      refCallback,
-      renderRow,
-      ...listViewProps
-    } = this.props;
-
-    return (
-      <View style={[defaultStyles.verticalContainer, style]}>
-        {typeof renderHeader === 'function' && renderHeader()}
-        <ListView
-          {...listViewProps}
-          ref={refCallback}
-          style={[defaultStyles.listview, listViewStyle]}
-          dataSource={dataSource}
-          renderRow={renderRow}
-        />
-      </View>
-    );
-  }
-}
+export const DataTable = React.memo(({ renderRow, ...otherProps }) => (
+  <VirtualizedList
+    style={defaultStyles.virtualizedList}
+    renderItem={renderRow}
+    {...otherProps}
+  />
+))
 
 DataTable.propTypes = {
-  style: ViewPropTypes.style,
+  ...VirtualizedListPropTypes,
   listViewStyle: PropTypes.object,
   refCallback: PropTypes.func,
   renderHeader: PropTypes.func,
-  dataSource: PropTypes.object.isRequired,
   renderRow: PropTypes.func.isRequired,
-};
-DataTable.defaultProps = {
-  showsVerticalScrollIndicator: true,
-  scrollRenderAheadDistance: 5000,
-};
+}
+DataTable.defaultProps = {}
 
 const defaultStyles = StyleSheet.create({
-  verticalContainer: {
+  virtualizedList: {
     flex: 1,
   },
-  listView: {
-    flex: 1,
-  },
-});
+})
