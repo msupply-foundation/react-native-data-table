4e2d5f9ab3f6b96c0d7393b3ca5165f6f57b79f8
Chris Petty <christopher.m.petty@gmail.com>
2019-07-25 15:07:21 +1200

    Clean up comments

--- examples/demoTablesApp/src/AppFunctional.js
+++ examples/demoTablesApp/src/AppFunctional.js
@@ -12,15 +12,19 @@ import { DataTable } from './components/DataTable'
 import { Row } from './components/Row'
 import { Cell } from './components/Cell'
 
-const rowCount = 200
+// Configurable constants for demo data volume
+const rowCount = 10
 const columnCount = 4
+
 const baseData = []
 const baseColumns = []
 
+// Make columns definition used in demo app
 for (let index = 0; index < columnCount; index++) {
-  baseColumns.push({ key: `col${index}`, editable: false }) // index === columnCount - 1
+  baseColumns.push({ key: `col${index}`, editable: index === columnCount - 1 }) // index === columnCount - 1
 }
 
+// Generate data for use in demo app
 for (let index = 0; index < rowCount; index++) {
   const rowValues = {}
   baseColumns.forEach((column, columnIndex) => {
@@ -30,40 +34,7 @@ for (let index = 0; index < rowCount; index++) {
   baseData.push({ id: `r${index}`, ...rowValues })
 }
 
-/**
- * =============================================================================
- * Below is the stuff. I think we'll probably need to use table context to tidy
- * up keyExtractor and dataReducer. Then we're basically using redux LOL.
- *
- * # TODO:
- * - Context for reducer to tidy up?
- * - Styling/theme context (rather than passing through style props)?
- * - Different Cell types as individual components? Pros/cons? (e.g. EditableCell, TableButton...)
- * - So, every different cell type needs an reducer action?
- * - Custom Cells?
- * - How to drill down a row (UX guy pls HALP!!!)
- * - Horizontal scrolling support!
- * - Anything to do with extraData prop...? (calculated fields? Row states/highlighting!?)
- * - Is the dataDispatch prop easily given a redux dispatch? That'd be really nice.
- * - HeaderRow/Cells. ListHeaderComponent? Sort icons/render props? Adjustable widths?
- * - ListFooterComponent?
- * - Start taking performance metrics
- * - Optimising the table via default props for VirtualisedList (e.g. getItemLayout, maxToRenderPerBatch, updateCellsBatchingPeriod, removeClippedSubviews...)
- * - Unit testing table to ensure no regressions on what triggers rerenders
- *
- * # Notes
- * If and only if the table is bigger than the viewport (say 500 rows):
- * - sorting can hammer row rerender (quite shit with 5000 rows): Simply has to if data range is totally out of currently rendered viewport
- *
- * More than ~40 TextInputs in table (i.e. Editable column) causes crashes https://github.com/facebook/react-native/issues/17530#issuecomment-416367184
- *
- * There have been requests for better support of reactXP and react-native-web (e.g. aria accessibility).
- * I think that depending on what VirtualisedList is ported to, this may be a gross misuse
- * of this package. Should implement native HTML for web based IMO.
- * =============================================================================
- */
-
-const keyExtractor = item => item.id // TODO: Should be default prop
+const keyExtractor = item => item.id
 const dataReducer = (data, action) => {
   switch (action.type) {
     case 'editRowCell':
